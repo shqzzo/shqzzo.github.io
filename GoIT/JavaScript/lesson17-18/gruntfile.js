c98fc404f6e9abe04872f1af5364ec85f56dd70f
shqzzo <shqzzo@gmail.com>
2016-02-26 14:50:20 +0200

    Homework 17-18

---
+++ GoIT/JavaScript/lesson17-18/gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/src/*.js'],
+        dest: 'js/dist/main.js'
+      }
+    },
+    uglify: {
+      dist: {
+        src: ['js/dist/main.js'],
+        dest: 'js/dist/main.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
---
+++ GoIT/JavaScript/lesson17-18/js/dist/main.js
@@ -0,0 +1,2 @@
+console.log('Script 2');
+;console.log('Script 2');
